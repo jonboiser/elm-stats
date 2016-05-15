e7f8ce9d75edf2e871e0cedfcc16d1b72e23b8ec
Jonathan Boiser <jboiser@gmail.com>
2016-05-14 19:39:03 -0500

    Stub out Distribution.randomEvent

--- src/Stats/Distribution.elm
+++ src/Stats/Distribution.elm
@@ -3,7 +3,32 @@ module Stats.Distribution exposing (..)
 
 type Distribution
     = Bernoulli Float
-    | Gaussian Float Float
     | Binomial Float Int
+    | Gaussian Float Float
     | Poisson Float
     | Uniform Float Float
+
+
+type Event
+    = Binary Bool
+    | Continuous Float
+    | Discrete Int
+
+
+randomEvent : Distribution -> Event
+randomEvent dist =
+    case dist of
+        Bernoulli p ->
+            Binary True
+
+        Binomial p n ->
+            Discrete 0
+
+        Gaussian mu sigma ->
+            Continuous 0
+
+        Poisson lambda ->
+            Continuous 0
+
+        Uniform a b ->
+            Continuous 0
